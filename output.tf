1163f8c5855e7be7a6ca3b7bb04a58b908e4c632
AKIN KARAKAYA <42320600+jasonbournes@users.noreply.github.com>
2022-10-12 16:03:59 +0300

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,8 @@
-# output "azurerg" {
-#   value = "nameoftheresourcegroup-${azurerm_resource_group.vmrg.name}-at${formatdate("DD MMM YYYY hh:mm ZZZ", timestamp())}"
-# }
-# #
-# output "azurerg1" {
-#   value = "id-${azurerm_resource_group.vmrg.id}"
-# }
+output "instance_ami" {
+  value = aws_instance.ubuntu.ami
+}
+
+output "instance_arn" {
+  value = aws_instance.ubuntu.arn
+}
+
