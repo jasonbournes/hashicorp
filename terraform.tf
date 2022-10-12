325306964ad3cb68ab5692709bb5820c565f810f
AKIN KARAKAYA <42320600+jasonbournes@users.noreply.github.com>
2022-10-12 16:12:07 +0300

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,19 +1,17 @@
 terraform {
 
   cloud {
-    organization = "organization-name"
+    organization = "zioncity"
 
     workspaces {
-      name = "learn-terraform-cloud"
+      name = "zion"
     }
   }
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.28.0"
+     
     }
   }
-
-  required_version = ">= 0.14.0"
 }
